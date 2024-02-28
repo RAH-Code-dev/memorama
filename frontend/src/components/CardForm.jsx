24e39c8afe64fde72ba45d74d28248079a264ebd
milosnowcat <ramiro.alvarez@rahcode.com>
2024-02-27 20:56:36 -0600

    export game

--- frontend/src/components/CardForm.jsx
+++ frontend/src/components/CardForm.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
-const CardForm = () => {
+const CardForm = ({ index, card, onCardChange }) => {
+  const handleQuestionChange = (e) => {
+    onCardChange(index, "question", e.target.value);
+  };
+
+  const handleAnswerChange = (e) => {
+    onCardChange(index, "answer", e.target.value);
+  };
+
   return (
     <div>
       <label>
-        Question:
-        <input type="text" />
+        Question:{" "}
+        <input
+          type="text"
+          value={card.question}
+          onChange={handleQuestionChange}
+        />
       </label>
       <label>
-        Answer:
-        <input type="text" />
+        Answer:{" "}
+        <input type="text" value={card.answer} onChange={handleAnswerChange} />
       </label>
     </div>
   );
--- frontend/src/components/GameForm.jsx
+++ frontend/src/components/GameForm.jsx
@@ -4,25 +4,47 @@ import React, { useState } from "react";
 import CardForm from "./CardForm";
 
 const GameForm = () => {
+  const [gameName, setGameName] = useState("");
   const [cards, setCards] = useState([]);
+
+  const handleGameNameChange = (e) => {
+    setGameName(e.target.value);
+  };
 
   const handleAddCard = () => {
     setCards([...cards, { question: "", answer: "" }]);
   };
 
+  const handleCardChange = (index, field, value) => {
+    const updatedCards = [...cards];
+    updatedCards[index][field] = value;
+    setCards(updatedCards);
+  };
+
   return (
     <div>
       <label>
-        Game Name:
-        {' '}
-        <input type="text" />
+        Game Name:{" "}
+        <input type="text" value={gameName} onChange={handleGameNameChange} />
       </label>
       <button onClick={handleAddCard}>Add Card</button>
       {cards.map((card, index) => (
-        <CardForm key={index} card={card}></CardForm>
+        <CardForm
+          key={index}
+          index={index}
+          card={card}
+          onCardChange={handleCardChange}
+        />
       ))}
+      <button onClick={() => exportGameData(gameName, cards)}>
+        Export Data
+      </button>
     </div>
   );
+};
+
+const exportGameData = (gameName, cards) => {
+  console.log("Exporting Game Data:", gameName, cards);
 };
 
 export default GameForm;
