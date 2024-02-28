d87980b78501d234725ada3fe9f0a6c58f6ec716
Cubos <cubos2769.andrade@gmail.com>
2024-02-27 22:11:02 -0600

    CSS de Board y solucion a bugs

--- frontend/src/app/board/page.jsx
+++ frontend/src/app/board/page.jsx
@@ -1,13 +1,45 @@
-"use client"
+"use client";
 
-import BoardComponent from "@/components/Board"
+// import AsideListComponent from "@/components/AsideListComponent";
+import BoardComponent from "@/components/Board";
+import styles from "@/styles/pages/Board.module.css";
 
 const BoardPage = () => {
-    return (
-    <div>
-        <BoardComponent/>
-    </div>
-    )
-}
+  let usuarios = [
+    {
+      nombre: "Cubos",
+      score: 0,
+    },
+    {
+      nombre: "Nestor",
+      score: 0,
+    },
+    {
+      nombre: "Angel",
+      score: 0,
+    },
+    {
+      nombre: "Milo",
+      score: 0,
+    },
+  ];
 
-export default BoardPage
+  return (
+    <>
+      <aside className={styles.aside}>
+        <p className={styles.title}>Nombre del equipo</p>
+        {usuarios.map((usuario) => (
+          <div className={styles.div}>
+            <p className={styles.p}>nombre: {usuario.nombre}</p>
+            <p className={styles.p}>Score: {usuario.score}</p>
+          </div>
+        ))}
+      </aside>
+      <div>
+        <BoardComponent />
+      </div>
+    </>
+  );
+};
+
+export default BoardPage;
