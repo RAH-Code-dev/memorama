2951d7197e9abac268747cabcfc039d8113350ef
milosnowcat <ramiro.alvarez@rahcode.com>
2024-02-27 20:39:19 -0600

    create new cards

--- frontend/src/components/GameForm.jsx
+++ frontend/src/components/GameForm.jsx
@@ -1,14 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
 import CardForm from "./CardForm";
 
 const GameForm = () => {
+  const [cards, setCards] = useState([]);
+
+  const handleAddCard = () => {
+    setCards([...cards, { question: "", answer: "" }]);
+  };
+
   return (
     <div>
       <label>
         Game Name:
+        {' '}
         <input type="text" />
       </label>
-      <button>Add Card</button>
-      <CardForm></CardForm>
+      <button onClick={handleAddCard}>Add Card</button>
+      {cards.map((card, index) => (
+        <CardForm key={index} card={card}></CardForm>
+      ))}
     </div>
   );
 };
