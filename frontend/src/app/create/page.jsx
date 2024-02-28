52b2c476ff7a4512318e5743529979bceaf2a69b
milosnowcat <ramiro.alvarez@rahcode.com>
2024-02-27 20:22:58 -0600

    GameForm

--- frontend/src/app/create/page.jsx
+++ frontend/src/app/create/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import GameForm from "@/components/GameForm";
 
 const Create = () => {
   return (
     <div>
       <h1>Create a New Game</h1>
+      <GameForm></GameForm>
     </div>
   );
 };
---
+++ frontend/src/components/GameForm.jsx
@@ -0,0 +1,13 @@
+const GameForm = () => {
+  return (
+    <div>
+      <label>
+        Game Name:
+        <input type="text" />
+      </label>
+      <button>Add Card</button>
+    </div>
+  );
+};
+
+export default GameForm;
