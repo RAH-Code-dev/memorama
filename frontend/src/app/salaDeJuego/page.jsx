1e25164a9459bc7c3d1f82bf8ca0660ab8532d5e
NestorNey <98431315+NestorNey@users.noreply.github.com>
2024-04-02 22:49:18 -0600

    Unnecessary import removed

--- frontend/src/app/salaDeJuego/page.jsx
+++ frontend/src/app/salaDeJuego/page.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import '@/styles/pages/gameView.css'
 import NeonCard from '@/components/neonCard'
 import LeftBarGame from '@/components/leftBar-gameView'
@@ -21,7 +20,7 @@ const GameView = () => {
                 })}
 
             </div>
-            
+
             {/* <InfoCard
         cardName = "Mantis Religiosa"
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis tellus in massa pretium, vitae cursus augue tristique. Aenean rutrum enim aliquam, malesuada enim id, mollis sem. Sed porta vehicula mauris, nec dapibus libero semper eu. Sed sit amet massa elementum, placerat sapien a, cursus eros. Vestibulum maximus purus enim, eu tristique nunc commodo non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus lacus sed neque aliquam pretium. Ut id risus nulla. Etiam non sodales velit. Suspendisse suscipit nulla libero, eget iaculis nibh imperdiet at. Integer ac nunc lectus. Cras eu augue turpis. Donec sed ultricies diam. Suspendisse porta sollicitudin ligula vitae lacinia. Aliquam ac magna lobortis, blandit dui at, pharetra massa."
