d21855d52e55f3e7e8cb3306b55f5550590d45f0
Nestor Emmanuel Ascencio Sanchez <nestor.ascencio1418@alumnos.udg.mx>
2024-04-05 10:31:49 -0600

    buttons style of 'crear' view modified

--- frontend/src/components/host/CardForm.jsx
+++ frontend/src/components/host/CardForm.jsx
@@ -1,42 +1,47 @@
 import styles from "@/styles/components/host/CardForm.module.css";
+import MainButton from "../MainButton";
 
 const CardForm = ({ index, card, onCardChange, onDeleteCard }) => {
-  const handleQuestionChange = (e) => {
-    onCardChange(index, "question", e.target.value);
-  };
+    const handleQuestionChange = (e) => {
+        onCardChange(index, "question", e.target.value);
+    };
 
-  const handleAnswerChange = (e) => {
-    onCardChange(index, "answer", e.target.value);
-  };
+    const handleAnswerChange = (e) => {
+        onCardChange(index, "answer", e.target.value);
+    };
 
-  return (
-    <div className={styles.cardContainer}>
-      <label className={styles.labelInput}>
-        <input
-          type="text"
-          value={card.question}
-          onChange={(e) => onCardChange(index, "question", e.target.value)}
-          className={styles.input_text}
-          placeholder="Pregunta"
-        />
-      </label>
-      <label className={styles.labelInput}>
-        <input
-          type="text"
-          value={card.answer}
-          onChange={(e) => onCardChange(index, "answer", e.target.value)}
-          className={styles.input_text}
-          placeholder="Respuesta"
-        />
-      </label>
-      <button
-        className={styles.deleteButton}
-        onClick={() => onDeleteCard(index)}
-      >
-Eliminar carta      
-</button>
-    </div>
-  );
+    return (
+        <div className={styles.cardContainer}>
+            <label className={styles.labelInput}>
+                <input
+                    type="text"
+                    value={card.question}
+                    onChange={(e) =>
+                        onCardChange(index, "question", e.target.value)
+                    }
+                    className={styles.input_text}
+                    placeholder="Pregunta"
+                />
+            </label>
+            <label className={styles.labelInput}>
+                <input
+                    type="text"
+                    value={card.answer}
+                    onChange={(e) =>
+                        onCardChange(index, "answer", e.target.value)
+                    }
+                    className={styles.input_text}
+                    placeholder="Respuesta"
+                />
+            </label>
+
+            <MainButton
+                onclick={() => onDeleteCard(index)}
+                msg="Eliminar carta"
+                level={4}
+            />
+        </div>
+    );
 };
 
 export default CardForm;
