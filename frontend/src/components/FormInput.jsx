c42994c60b3dd766c51e2242ff1bf5ecb4657a3e
Nestor Emmanuel Ascencio Sanchez <nestor.ascencio1418@alumnos.udg.mx>
2024-04-05 10:56:25 -0600

    ClassName prop added to FormInput

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,9 +1,9 @@
 import style from '@/styles/components/FormInput.module.css'
 
-export default function FormInput( { type, name, placeholder } ) {
+export default function FormInput( { type, name, placeholder, className } ) {
   return (
     <input
-      className={`${style.FormInput}`}
+      className={`${style.FormInput} ${className}`}
       type={type}
       name={name}
       placeholder={placeholder}
