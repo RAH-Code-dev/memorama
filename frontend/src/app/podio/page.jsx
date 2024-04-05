bc0b81853f6af8036fec3f7d6018460544477100
Nestor Emmanuel Ascencio Sanchez <nestor.ascencio1418@alumnos.udg.mx>
2024-04-05 10:37:03 -0600

    buttons style of 'podio' view modified

--- frontend/src/app/podio/page.jsx
+++ frontend/src/app/podio/page.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
-import Podio from '@/components/player/Podio'
-import style from '@/styles/pages/podioPage.module.css'
-import Link from 'next/link'
+"use client";
+
+import React from "react";
+import Podio from "@/components/player/Podio";
+import style from "@/styles/pages/podioPage.module.css";
+import Link from "next/link";
+import MainButton from "@/components/MainButton";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  return (
-    <section className={style.stats}>
-      <h1 className={style.h1winners}>Ganadores</h1>
-      <h1 className={style.h1felicidades}>Felicidades</h1>
-      <section className={style.podium}>
+    return (
+        <section className={style.stats}>
+            <h1 className={style.h1winners}>Ganadores</h1>
+            <h1 className={style.h1felicidades}>Felicidades</h1>
+            <section className={style.podium}>
+                <Podio usuario="Luis" lugar="2" duracion={3} />
+                <Podio usuario="Miguel" lugar="1" duracion={2} />
+                <Podio usuario="Ana" lugar="3" duracion={4} />
+            </section>
 
-        <Podio
-          usuario="Luis"
-          lugar="2"
-          duracion={3}
-        />
-        <Podio
-          usuario="Miguel"
-          lugar="1"
-          duracion={2}
-        />
-        <Podio
-          usuario="Ana"
-          lugar="3"
-          duracion={4}
-        />
-
-      </section>
-
-      <Link className={style.ctaPodium} href="/formIngreso">Volver al registro</Link>
-
-    </section>
-  )
+            <MainButton
+                className={style.ctaPodium}
+                onclick={() => {
+                    useRouter().push("/formIngreso");
+                }}
+                msg="Volver al registro"
+                level={1}
+            />
+        </section>
+    );
 }
